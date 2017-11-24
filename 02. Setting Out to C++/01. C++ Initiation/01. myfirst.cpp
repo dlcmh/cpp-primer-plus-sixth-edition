918f839a98c6e626e53a1743a68ef984fd43daf5
David Chin <dlcmhd@me.com>
2017-11-24 16:02:59 +0800

    commit

--- 02. Setting Out to C++/01. C++ Initiation/01. myfirst.cpp
+++ 02. Setting Out to C++/01. C++ Initiation/01. myfirst.cpp
@@ -3,7 +3,13 @@
 #include <iostream> // a PREPROCESSOR directive
 
 int main() { // function header; { starts function body
-  using namespace std; // make definitions visible; using a directive
+
+  // make definitions visible; using a directive
+  // lazy approach, all name available
+  using namespace std;
+  // alternatively ...
+  // using std::cout;
+  // using std::endl;
 
   cout << "Come up and C++ me some time."; // message
   cout << endl; // start a new line
