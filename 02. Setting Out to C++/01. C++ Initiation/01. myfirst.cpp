07373aefd539d70aa3e2b7cf6ff94661cd56fa79
David Chin <dlcmhd@me.com>
2017-11-24 15:15:53 +0800

    commit

---
+++ 02. Setting Out to C++/01. C++ Initiation/01. myfirst.cpp
@@ -0,0 +1,13 @@
+// displays a message
+
+#include <iostream>
+
+int main() {
+  using namespace std;
+
+  cout << "Come up and C++ me some time.";
+  cout << endl;
+  cout << "You wont't regret it!" << endl;
+
+  return 0;
+}
